e019a0196cb3c08ecbab159429505b444ee6b54b
毅杨 <yangyi@gogopay.cn>
2019-01-04 18:24:22 +0800

    [Feature]提交登录代码

---
+++ src/main/java/com/mmall/service/impl/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package com.mmall.service.impl;
+
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+import com.mmall.service.IUserService;
+
+public class UserServiceImpl implements IUserService {
+    @Override
+    public ServerResponse<User> login(String username, String password) {
+        return null;
+    }
+}
